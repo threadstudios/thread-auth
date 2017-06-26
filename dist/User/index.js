1e556e5089c82b7ebb9724cf686c94ea271d52ff
Paul Westerdale <paul@westerdale.me>
2017-06-26 10:27:39 +0100

    correctly reference Saga in exports

--- dist/User/index.js
+++ dist/User/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.Mapper = exports.Repo = exports.User = undefined;
+exports.Saga = exports.Mapper = exports.Repo = exports.User = undefined;
 
 var _Repo = require('./Repo');
 
@@ -24,8 +24,9 @@ var _Saga2 = _interopRequireDefault(_Saga);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-    User: _User2.default, Repo: _Repo2.default, Mapper: _Mapper2.default
+    User: _User2.default, Repo: _Repo2.default, Mapper: _Mapper2.default, Saga: _Saga2.default
 };
 exports.User = _User2.default;
 exports.Repo = _Repo2.default;
 exports.Mapper = _Mapper2.default;
+exports.Saga = _Saga2.default;
--- src/User/index.js
+++ src/User/index.js
@@ -4,9 +4,9 @@ import User from './User.js';
 import Saga from './Saga';
 
 export default {
-    User, Repo, Mapper
+    User, Repo, Mapper, Saga
 }
 
 export {
-    User, Repo, Mapper
+    User, Repo, Mapper, Saga
 }
